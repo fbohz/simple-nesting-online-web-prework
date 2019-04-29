78c0e2be42828c90f9bafc817c2cf0f057223360
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-29 21:03:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,13 +84,14 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         },
-       :yukihiro_matsumoto => {
-             :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-       }
+
      }
 
 
+		 programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
 end
 
 def changing_alan
@@ -112,6 +113,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -133,4 +135,5 @@ def adding_to_dennis
      }
 
   programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  programmer_hash
 end
